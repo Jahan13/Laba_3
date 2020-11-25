9e73ed1cd0d3cf2e0d9064fda895e7d1b5b398c7
Jahan Ovezova <j4hanovezova@yandex.ru>
2020-11-25 11:42:10 +0300

    Added:class fields

--- src/bsu/rfct/java/Laba_3/GornerTableCellRenderer.java
+++ src/bsu/rfct/java/Laba_3/GornerTableCellRenderer.java
@@ -39,4 +39,43 @@ public class GornerTableCellRenderer  {
 
         panel.setLayout(new FlowLayout(FlowLayout.LEFT));
     }
+    public Component getTableCellRendererComponent(JTable table,Object value,boolean isSelected,boolean hasFocus,int row,int col){
+
+        String formattedDouble = formatter.format(value);
+
+        int index = formattedDouble.indexOf('.');
+        String str1="1";
+        String str3="3";
+        String str5="5";
+        int index1=formattedDouble.indexOf('1',index);
+        int index3=formattedDouble.indexOf('3',index);
+        int index5=formattedDouble.indexOf('5',index);
+
+
+
+
+        label.setText(formattedDouble);
+        if (col==1 && needle!=null && needle.equals(formattedDouble)) {
+
+
+
+
+            panel.setBackground(Color.RED);
+        } else {
+
+            panel.setBackground(Color.WHITE);
+        }
+
+        if(index1!=-1 || index3!=-1 || index5!=-1 ) {
+            if(str1.indexOf('1',index)==-1 && str3.indexOf('3',index)==-1 && str5.indexOf('5',index)==-1) panel.setBackground(Color.ORANGE);}
+
+        //if(sub.contains("1")||sub.contains("3")||sub.contains("5")){
+        //   if(sub!="1" && sub!="3" && sub!="5") panel.setBackground(Color.ORANGE);}
+        return panel;
+
+    }
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
 }
+
